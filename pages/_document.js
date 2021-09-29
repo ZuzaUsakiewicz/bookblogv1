f9ba503e731ff278a17c518378873d8c4fc336e8
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-09-29 15:49:51 +0200

    add font

--- pages/_document.js
+++ pages/_document.js
@@ -31,16 +31,12 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;1,300&family=Prata&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400&display=swap"
             rel="stylesheet"
           />
-          <link
-            href="https://api.fontshare.com/css?f[]=satoshi@300&display=swap"
-            rel="stylesheet"
-          />
-         */}
         </Head>
         <body>
           <Main />
