db0b8cb90a217df701b3b65031dd60c790bca8a1
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-09-29 15:48:46 +0200

    add global styles, layout and theme for app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import Layout from "../components/Layout";
+import Theme from "../components/Theme";
+import * as styled from "styled-components";
+
+const GlobalStyle = styled.createGlobalStyle`
+  *,
+  html {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    font-family: "Mukta", sans-serif;
+    font-weight: 200;
+    font-size: 20px;
+    line-height: 1.4rem;
+    color: ${({ theme }) => theme.colors.primaryFont};
+    background-color: ${({ theme }) => theme.colors.backgroundColor};
+  }
+
+  button {
+    cursor: pointer;
+    background: transparent;
+    border: none;
+    outline: none;
+    font-family: Satoshi;
+    &:focus-visible {
+      outline: 2px solid black;
+    }
+  }
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Theme>
+      <GlobalStyle />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Theme>
+  );
 }
 
-export default MyApp
+export default MyApp;
