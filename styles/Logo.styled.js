8adacb6096ee58b0a10d8e8c428e3c273c405288
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-10-16 19:52:22 +0200

    add logo component styles

---
+++ styles/Logo.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const LogoContainer = styled.div`
+  position: relative;
+  width: 8vw;
+  height: 8vh;
+  padding: 0.5rem 1rem;
+`;
+
+export const LogoImage = styled(Image)``;
