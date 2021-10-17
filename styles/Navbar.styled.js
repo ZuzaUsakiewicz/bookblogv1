570c6e6ef6763a64527edee597851119e4abe240
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-10-17 11:29:12 +0200

    add navbar styling

---
+++ styles/Navbar.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Navbar = styled.header`
+  width: 100%;
+  height: 8vh;
+  background: rgb(255, 255, 255);
+  display: flex;
+  align-items: center;
+  padding: 0 3rem;
+  position: fixed;
+  top: 0;
+  z-index: 999;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+`;
