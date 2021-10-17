92bfea52d7dfa2d17777b6973eaa746c9d884fe5
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-10-17 11:28:48 +0200

    create header component

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import { Navbar } from "../styles/Navbar.styled";
+import Logo from "../components/Logo";
+
+export default function Header() {
+  return (
+    <>
+      <Navbar>
+        <Logo />
+      </Navbar>
+    </>
+  );
+}
