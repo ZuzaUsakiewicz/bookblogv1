be629509466908cb5ad7fa8821bfb7e9f5242633
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-10-17 11:33:30 +0200

    add link to homepage for logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,16 +1,19 @@
 import { LogoContainer, LogoImage } from "../styles/Logo.styled";
 import LogoBook from "../public/zetlogobook.svg";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <LogoContainer>
-      <LogoImage
-        src={LogoBook}
-        alt="personal logo"
-        layout="fill"
-        objectFit="contain"
-        quality={100}
-      ></LogoImage>
-    </LogoContainer>
+    <Link href={`/`} passHref>
+      <LogoContainer>
+        <LogoImage
+          src={LogoBook}
+          alt="personal logo"
+          layout="fill"
+          objectFit="contain"
+          quality={100}
+        ></LogoImage>
+      </LogoContainer>
+    </Link>
   );
 }
